a211d0135860ab3006d4d743a27cff05a1976617
Nick Jordan <nick@jorcademy.nl>
2022-10-24 16:29:59 +0200

    [REFACTOR] Refactored Board class.

--- Mankala/Board.cs
+++ Mankala/Board.cs
@@ -9,39 +9,50 @@ namespace Mankala
 {
     public class Board
     {
-        // Contains pits in board
+        // Constant values
+        private const int OFFSET_TO_BORDER = 20;
+        private const int PIT_OFFSET = 10;
+
+        // Array containing pits in board
         Pit[] pits;
 
-        // Board properties
-        private Point coords; 
+        // Pit dimensions
+        private int homePitWidth;
+        private int homePitHeight;
+        private int playPitWidth;
+        private int playPitHeight;
+
+        // Board dimensions
         public int Size { get; set; }
+        private Point coords;
         private int width;
         private int height;
-        private int offsetToBorder = 20;
-        private const int pitOffset = 10;
 
         public Board(int size)
         {
+            // Init board size
             this.Size = size;
 
+            // Arrange array of pits
             this.pits = new Pit[Size + 2];
             this.FillBoard();
 
-            this.width = (pits[0].Width * 2) + (pits[0].Width * this.Size) + (pitOffset * (this.Size + 1)) + (2 * offsetToBorder);
-            this.height = (pits[0].Height) + this.Size + offsetToBorder * 2;
+            // Init pit sizes
+            this.homePitWidth = pits[0].Width;
+            this.homePitHeight = pits[0].Height;
+            this.playPitWidth = pits[1].Width;
+            this.playPitHeight = pits[1].Height;
 
+            // Set dimensions of board 
+            this.width = (pits[0].Width * 2) + (pits[0].Width * this.Size) + (PIT_OFFSET * (this.Size + 1)) + (2 * OFFSET_TO_BORDER);
+            this.height = (pits[0].Height) + this.Size + OFFSET_TO_BORDER * 2;
+
+            // Set coordinates of board
             this.coords.X = (800 - this.width) / 2;
-            this.coords.Y = 200;
+            this.coords.Y = (600 - this.height) / 2;
         }
 
-        public void Draw(Graphics gr)
-        {
-            DrawBoardBackdrop(gr);
-            DrawHomePits(gr);
-            DrawPlayPits(gr);
-        }
-
-        /* Fill board */
+        /* Fill board with pits */
         private void FillBoard()
         {
             for (int i = 0; i < (Size + 1); i++)
@@ -57,43 +68,44 @@ namespace Mankala
             }
         }
 
+        /* Draw the full board */
+        public void Draw(Graphics gr)
+        {
+            DrawBoardBackdrop(gr);
+            DrawHomePits(gr);
+            DrawPlayPits(gr);
+        }
+
         /* Draw backdrop of the board */
         public void DrawBoardBackdrop(Graphics gr)
         {
             // Color: Brushes.Peru
-            gr.FillRectangle(Brushes.Peru, coords.X, coords.Y - offsetToBorder, this.width, this.height);
+            gr.FillRectangle(Brushes.Peru, coords.X, coords.Y - OFFSET_TO_BORDER, this.width, this.height);
         }
 
-        /* Drawing play pits */
+        /* Drawing home pits */
         private void DrawHomePits(Graphics gr)
         {
-            // Pit dimensions
-            int homeWidth = 80;
-            int homeHeight = 170;
+            // Draw left home pit
+            gr.FillEllipse(Brushes.Sienna, OFFSET_TO_BORDER + coords.X, coords.Y, homePitWidth, homePitHeight);
 
-            // Draw first home pit
-            gr.FillEllipse(Brushes.Sienna, offsetToBorder + coords.X, coords.Y, homeWidth, homeHeight);
-
-            // Draw second home pit
-            gr.FillEllipse(Brushes.Sienna, offsetToBorder + coords.X + pitOffset + homeWidth + homeWidth * Size + pitOffset * Size, coords.Y, homeWidth, homeHeight);
+            // Draw right home pit
+            gr.FillEllipse(Brushes.Sienna, OFFSET_TO_BORDER + coords.X + PIT_OFFSET + homePitWidth + homePitWidth * Size + PIT_OFFSET * Size, coords.Y, homePitWidth, homePitHeight);
         }
 
         /* Drawing play pits */
         private void DrawPlayPits(Graphics gr)
         {
-            // Pit dimensions
-            int playWidth = 80;
-            int playHeight = 80;
-
-            // Draw play pits
+            // Draw first row
             for (int i = 0; i < Size; i++)
             {
-                gr.FillEllipse(Brushes.Sienna, offsetToBorder + coords.X + playWidth + pitOffset + playWidth * i + pitOffset * i, coords.Y, playWidth, playHeight);
+                gr.FillEllipse(Brushes.Sienna, OFFSET_TO_BORDER + coords.X + playPitWidth + PIT_OFFSET + playPitWidth * i + PIT_OFFSET * i, coords.Y, playPitWidth, playPitHeight);
             }
 
+            // Draw secod row
             for (int i = 0; i < Size; i++)
             {
-                gr.FillEllipse(Brushes.Sienna, offsetToBorder + coords.X + playWidth + pitOffset + playWidth * i + pitOffset * i, coords.Y + playWidth + pitOffset, playWidth, playHeight);
+                gr.FillEllipse(Brushes.Sienna, OFFSET_TO_BORDER + coords.X + playPitWidth + PIT_OFFSET + playPitWidth * i + PIT_OFFSET * i, coords.Y + playPitWidth + PIT_OFFSET, playPitWidth, playPitHeight);
             }
         }
     }
